12b11b685943c32c54b7b9526f6cd931eaeb5305
MrCat2357 <catuloaxel.mat@gmail.com>
2026-05-02 15:48:20 -0300

    fix firebase config with env vars

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4VndXA9ohPCK4vKA-xx-PBdlAx66KUsA",
-  authDomain: "vozdafe-site.firebaseapp.com",
-  projectId: "vozdafe-site",
-  storageBucket: "vozdafe-site.firebasestorage.app",
-  messagingSenderId: "639470843740",
-  appId: "1:639470843740:web:0cd6d940dcd48021fdb399",
-  measurementId: "G-RRTGNMXX1L"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
